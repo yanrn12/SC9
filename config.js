28bb98bfe033b7c67f4b6b4dda3067b60fc64925
yanrn12 <107698772+yanrn12@users.noreply.github.com>
2022-08-16 19:27:32 +0700

    Update config.js

--- config.js
+++ config.js
@@ -12,10 +12,10 @@ global.APIKeys = {
 }
 
 // NAMBAH BOLEH HAPUS JANGAN -_-
-global.owner = ['6281511480762']
-global.premium = ['621511480762']
-global.packname = 'AzzBot√'
-global.author = 'AzzBot-MD'
+global.owner = ['6289516947204']
+global.premium = ['6289516947204']
+global.packname = 'ʀᴄᴛɪ ʙᴏᴛ√'
+global.author = 'ʀᴄᴛɪ ʙᴏᴛ-MD'
 global.sessionName = 'Xavior'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
